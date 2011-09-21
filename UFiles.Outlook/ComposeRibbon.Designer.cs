05d184bf161a3514d66ae694cd471b2fa06b8075
Daniel <Daniel@localhost>
2011-09-21 22:53:46 +1000

    neglected to upload earlier; many additions to comments and backend

--- UFiles.Outlook/ComposeRibbon.Designer.cs
+++ UFiles.Outlook/ComposeRibbon.Designer.cs
@@ -11,7 +11,11 @@
             : base(Globals.Factory.GetRibbonFactory())
         {
             InitializeComponent();
+
         }
+
+
+
 
         /// <summary> 
         /// Clean up any resources being used.
@@ -38,21 +42,40 @@
             this.manageuFile = this.Factory.CreateRibbonGroup();
             this.upload = this.Factory.CreateRibbonButton();
             this.addLink = this.Factory.CreateRibbonButton();
+            this.deleteButton = this.Factory.CreateRibbonButton();
+            this.separator2 = this.Factory.CreateRibbonSeparator();
+            this.uFileMenu = this.Factory.CreateRibbonMenu();
             this.manageAccount = this.Factory.CreateRibbonGroup();
-            this.userStatus = this.Factory.CreateRibbonLabel();
             this.changeUser = this.Factory.CreateRibbonButton();
-            this.signOut = this.Factory.CreateRibbonButton();
-            this.serverIP = this.Factory.CreateRibbonEditBox();
-            this.uploaduFile = new System.Windows.Forms.OpenFileDialog();
-            this.signIn = this.Factory.CreateRibbonButton();
+            this.signOutButton = this.Factory.CreateRibbonButton();
+            this.signInButton = this.Factory.CreateRibbonButton();
             this.separator1 = this.Factory.CreateRibbonSeparator();
+            this.uploaduFile = new System.Windows.Forms.OpenFileDialog();
+            this.uFileRestrictions = this.Factory.CreateRibbonTab();
+            this.restrictionsGroup = this.Factory.CreateRibbonGroup();
+            this.presetDropDown = this.Factory.CreateRibbonDropDown();
+            this.savePresetButton = this.Factory.CreateRibbonButton();
+            this.deletePresetButton = this.Factory.CreateRibbonButton();
+            this.timeRestrictionGroup = this.Factory.CreateRibbonGroup();
+            this.otherRestrictionGroup = this.Factory.CreateRibbonGroup();
+            this.ipRestrictionComboBox = this.Factory.CreateRibbonComboBox();
+            this.locationMenu = this.Factory.CreateRibbonMenu();
+            this.groupMenu = this.Factory.CreateRibbonMenu();
             this.box1 = this.Factory.CreateRibbonBox();
-            this.userName = this.Factory.CreateRibbonLabel();
-            this.userNameOutput = this.Factory.CreateRibbonLabel();
+            this.serverIP = this.Factory.CreateRibbonEditBox();
+            this.serverConnectlabel = this.Factory.CreateRibbonLabel();
+            this.box2 = this.Factory.CreateRibbonBox();
+            this.userStatus = this.Factory.CreateRibbonLabel();
+            this.usernameEditBox = this.Factory.CreateRibbonEditBox();
+            this.passwordEditBox = this.Factory.CreateRibbonEditBox();
             this.uFile.SuspendLayout();
             this.manageuFile.SuspendLayout();
             this.manageAccount.SuspendLayout();
+            this.uFileRestrictions.SuspendLayout();
+            this.restrictionsGroup.SuspendLayout();
+            this.otherRestrictionGroup.SuspendLayout();
             this.box1.SuspendLayout();
+            this.box2.SuspendLayout();
             // 
             // uFile
             // 
@@ -65,6 +88,9 @@
             // 
             this.manageuFile.Items.Add(this.upload);
             this.manageuFile.Items.Add(this.addLink);
+            this.manageuFile.Items.Add(this.deleteButton);
+            this.manageuFile.Items.Add(this.separator2);
+            this.manageuFile.Items.Add(this.uFileMenu);
             this.manageuFile.Label = "Manage uFiles";
             this.manageuFile.Name = "manageuFile";
             // 
@@ -76,27 +102,36 @@
             // 
             // addLink
             // 
-            this.addLink.Label = "Insert a uFile Link";
+            this.addLink.Label = "Insert uFile Link";
             this.addLink.Name = "addLink";
             this.addLink.ScreenTip = "Insert a link to an existing file";
+            this.addLink.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.addLink_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Label = "Delete File";
+            this.deleteButton.Name = "deleteButton";
+            // 
+            // separator2
+            // 
+            this.separator2.Name = "separator2";
+            // 
+            // uFileMenu
+            // 
+            this.uFileMenu.Label = "...";
+            this.uFileMenu.Name = "uFileMenu";
             // 
             // manageAccount
             // 
             this.manageAccount.Items.Add(this.changeUser);
-            this.manageAccount.Items.Add(this.signOut);
-            this.manageAccount.Items.Add(this.signIn);
+            this.manageAccount.Items.Add(this.signOutButton);
+            this.manageAccount.Items.Add(this.signInButton);
             this.manageAccount.Items.Add(this.separator1);
-            this.manageAccount.Items.Add(this.serverIP);
             this.manageAccount.Items.Add(this.box1);
-            this.manageAccount.Items.Add(this.userStatus);
+            this.manageAccount.Items.Add(this.box2);
+            this.manageAccount.Items.Add(this.passwordEditBox);
             this.manageAccount.Label = "Manage Account";
             this.manageAccount.Name = "manageAccount";
-            // 
-            // userStatus
-            // 
-            this.userStatus.Label = "Guest Account";
-            this.userStatus.Name = "userStatus";
-            this.userStatus.ScreenTip = "What type of account you are currently using";
             // 
             // changeUser
             // 
@@ -104,11 +139,94 @@
             this.changeUser.Name = "changeUser";
             this.changeUser.ScreenTip = "Change the user currently logged into uFile";
             // 
-            // signOut
+            // signOutButton
             // 
-            this.signOut.Label = "Sign Out";
-            this.signOut.Name = "signOut";
-            this.signOut.ScreenTip = "Sign Out";
+            this.signOutButton.Label = "Sign Out";
+            this.signOutButton.Name = "signOutButton";
+            this.signOutButton.ScreenTip = "Sign Out";
+            this.signOutButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.signOutButton_Click);
+            // 
+            // signInButton
+            // 
+            this.signInButton.Label = "Sign In";
+            this.signInButton.Name = "signInButton";
+            this.signInButton.ScreenTip = "Sign In";
+            this.signInButton.Visible = false;
+            // 
+            // separator1
+            // 
+            this.separator1.Name = "separator1";
+            // 
+            // uploaduFile
+            // 
+            this.uploaduFile.Title = "Upload uFile";
+            // 
+            // uFileRestrictions
+            // 
+            this.uFileRestrictions.Groups.Add(this.restrictionsGroup);
+            this.uFileRestrictions.Groups.Add(this.timeRestrictionGroup);
+            this.uFileRestrictions.Groups.Add(this.otherRestrictionGroup);
+            this.uFileRestrictions.Label = "uFile Restrictions";
+            this.uFileRestrictions.Name = "uFileRestrictions";
+            this.uFileRestrictions.Visible = false;
+            // 
+            // restrictionsGroup
+            // 
+            this.restrictionsGroup.Items.Add(this.presetDropDown);
+            this.restrictionsGroup.Items.Add(this.savePresetButton);
+            this.restrictionsGroup.Items.Add(this.deletePresetButton);
+            this.restrictionsGroup.Label = "File Restrictions";
+            this.restrictionsGroup.Name = "restrictionsGroup";
+            // 
+            // presetDropDown
+            // 
+            this.presetDropDown.Label = "Preset:";
+            this.presetDropDown.Name = "presetDropDown";
+            // 
+            // savePresetButton
+            // 
+            this.savePresetButton.Label = "Save Preset";
+            this.savePresetButton.Name = "savePresetButton";
+            // 
+            // deletePresetButton
+            // 
+            this.deletePresetButton.Label = "Delete Preset";
+            this.deletePresetButton.Name = "deletePresetButton";
+            // 
+            // timeRestrictionGroup
+            // 
+            this.timeRestrictionGroup.Label = "Time Restriction";
+            this.timeRestrictionGroup.Name = "timeRestrictionGroup";
+            // 
+            // otherRestrictionGroup
+            // 
+            this.otherRestrictionGroup.Items.Add(this.ipRestrictionComboBox);
+            this.otherRestrictionGroup.Items.Add(this.locationMenu);
+            this.otherRestrictionGroup.Items.Add(this.groupMenu);
+            this.otherRestrictionGroup.Label = "Other";
+            this.otherRestrictionGroup.Name = "otherRestrictionGroup";
+            // 
+            // ipRestrictionComboBox
+            // 
+            this.ipRestrictionComboBox.Label = "IP";
+            this.ipRestrictionComboBox.Name = "ipRestrictionComboBox";
+            this.ipRestrictionComboBox.Text = null;
+            // 
+            // locationMenu
+            // 
+            this.locationMenu.Label = "Location";
+            this.locationMenu.Name = "locationMenu";
+            // 
+            // groupMenu
+            // 
+            this.groupMenu.Label = "Group";
+            this.groupMenu.Name = "groupMenu";
+            // 
+            // box1
+            // 
+            this.box1.Items.Add(this.serverIP);
+            this.box1.Items.Add(this.serverConnectlabel);
+            this.box1.Name = "box1";
             // 
             // serverIP
             // 
@@ -117,43 +235,41 @@
             this.serverIP.ScreenTip = "Designate the IP/URL of the uFile server";
             this.serverIP.Text = null;
             // 
-            // uploaduFile
+            // serverConnectlabel
             // 
-            this.uploaduFile.Title = "Upload uFile";
+            this.serverConnectlabel.Label = "Disconnected";
+            this.serverConnectlabel.Name = "serverConnectlabel";
             // 
-            // signIn
+            // box2
             // 
-            this.signIn.Label = "Sign In";
-            this.signIn.Name = "signIn";
-            this.signIn.ScreenTip = "Sign In";
-            this.signIn.Visible = false;
+            this.box2.Items.Add(this.userStatus);
+            this.box2.Items.Add(this.usernameEditBox);
+            this.box2.Name = "box2";
             // 
-            // separator1
+            // userStatus
             // 
-            this.separator1.Name = "separator1";
+            this.userStatus.Label = "Guest Account";
+            this.userStatus.Name = "userStatus";
+            this.userStatus.ScreenTip = "What type of account you are currently using";
             // 
-            // box1
+            // usernameEditBox
             // 
-            this.box1.Items.Add(this.userName);
-            this.box1.Items.Add(this.userNameOutput);
-            this.box1.Name = "box1";
+            this.usernameEditBox.Label = "User Name";
+            this.usernameEditBox.Name = "usernameEditBox";
+            this.usernameEditBox.Text = null;
             // 
-            // userName
+            // passwordEditBox
             // 
-            this.userName.Label = "UserName:";
-            this.userName.Name = "userName";
-            // 
-            // userNameOutput
-            // 
-            this.userNameOutput.Label = "email@email.com";
-            this.userNameOutput.Name = "userNameOutput";
-            this.userNameOutput.ScreenTip = "Username";
+            this.passwordEditBox.Label = "  Password";
+            this.passwordEditBox.Name = "passwordEditBox";
+            this.passwordEditBox.Text = null;
             // 
             // ComposeRibbon
             // 
             this.Name = "ComposeRibbon";
             this.RibbonType = "Microsoft.Outlook.Mail.Compose";
             this.Tabs.Add(this.uFile);
+            this.Tabs.Add(this.uFileRestrictions);
             this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.ComposeRibbon_Load);
             this.uFile.ResumeLayout(false);
             this.uFile.PerformLayout();
@@ -161,8 +277,16 @@
             this.manageuFile.PerformLayout();
             this.manageAccount.ResumeLayout(false);
             this.manageAccount.PerformLayout();
+            this.uFileRestrictions.ResumeLayout(false);
+            this.uFileRestrictions.PerformLayout();
+            this.restrictionsGroup.ResumeLayout(false);
+            this.restrictionsGroup.PerformLayout();
+            this.otherRestrictionGroup.ResumeLayout(false);
+            this.otherRestrictionGroup.PerformLayout();
             this.box1.ResumeLayout(false);
             this.box1.PerformLayout();
+            this.box2.ResumeLayout(false);
+            this.box2.PerformLayout();
 
         }
 
@@ -173,16 +297,31 @@
         internal Microsoft.Office.Tools.Ribbon.RibbonButton upload;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton addLink;
         internal Microsoft.Office.Tools.Ribbon.RibbonGroup manageAccount;
-        internal Microsoft.Office.Tools.Ribbon.RibbonLabel userStatus;
-        internal Microsoft.Office.Tools.Ribbon.RibbonButton signOut;
-        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox serverIP;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton signOutButton;
         private System.Windows.Forms.OpenFileDialog uploaduFile;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton changeUser;
-        internal Microsoft.Office.Tools.Ribbon.RibbonButton signIn;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton signInButton;
         internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton deleteButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator2;
+        private Microsoft.Office.Tools.Ribbon.RibbonTab uFileRestrictions;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup restrictionsGroup;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown presetDropDown;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton savePresetButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton deletePresetButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup timeRestrictionGroup;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup otherRestrictionGroup;
+        internal Microsoft.Office.Tools.Ribbon.RibbonComboBox ipRestrictionComboBox;
+        internal Microsoft.Office.Tools.Ribbon.RibbonMenu locationMenu;
+        internal Microsoft.Office.Tools.Ribbon.RibbonMenu groupMenu;
+        internal Microsoft.Office.Tools.Ribbon.RibbonMenu uFileMenu;
         internal Microsoft.Office.Tools.Ribbon.RibbonBox box1;
-        internal Microsoft.Office.Tools.Ribbon.RibbonLabel userName;
-        internal Microsoft.Office.Tools.Ribbon.RibbonLabel userNameOutput;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox serverIP;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel serverConnectlabel;
+        internal Microsoft.Office.Tools.Ribbon.RibbonBox box2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel userStatus;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox usernameEditBox;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox passwordEditBox;
     }
 
     partial class ThisRibbonCollection
--- UFiles.Outlook/ComposeRibbon.cs
+++ UFiles.Outlook/ComposeRibbon.cs
@@ -8,14 +8,48 @@ namespace UFiles.Outlook
 {
     public partial class ComposeRibbon
     {
+        private String userName;
+        private String password;
+
         private void ComposeRibbon_Load(object sender, RibbonUIEventArgs e)
         {
             this.upload.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.upload_Click);
-            this.signOut.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.signOut_Click);
-            this.signIn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.signIn_Click);
+            this.signOutButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.signOut_Click);
+            this.signInButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.signIn_Click);
 
+
+            updateuFileGallery();
         }//Called when the ribbon is loaded
 
+
+        #region uFile ribbon section
+        #region misc. methods
+        private void updateuFileGallery()
+        {
+            string[] fileList = loadFileList();
+            RibbonDropDownItem RDDI;
+            if (fileList != null)
+            {
+                uFileMenu.Items.Clear();
+                for (int i = 0; i >= fileList.Length; i++)
+                {
+                    RDDI = this.Factory.CreateRibbonDropDownItem();
+                    RDDI.Label = fileList[i];
+                    uFileMenu.Items.Add((Microsoft.Office.Tools.Ribbon.RibbonControl)RDDI);
+                }
+            }
+        }
+        private string[] loadFileList()
+        {
+            return null;
+        }
+
+        private String signIn()
+        {
+            return "passed";
+        }
+
+        #endregion
         #region Event Handlers
 
         private void upload_Click(object sender, RibbonControlEventArgs e)
@@ -25,24 +59,88 @@ namespace UFiles.Outlook
         }//called when the uFile upload button is clicked
         void signOut_Click(object sender, RibbonControlEventArgs e)
         {
-            this.signOut.Visible = false;
-            this.signIn.Visible = true;
-            this.userNameOutput.Label = "";
+            this.signOutButton.Visible = false;
+            this.signInButton.Visible = true;
+            //Swap the sign in buttons
+
+            this.usernameEditBox.Text = "";
+            this.usernameEditBox.Enabled = true;
+            //clear and enable the username edit box
+
+            this.passwordEditBox.Text = "";
+            this.passwordEditBox.Enabled = true;
+            //clear and enable the apssword edit box
+
             this.userStatus.Label = "Not Signed In";
         }//called when the sign out button is clicked
-
         void signIn_Click(object sender, RibbonControlEventArgs e)
         {
-            this.signIn.Visible = false;
-            this.signOut.Visible = true;
-            this.userNameOutput.Label = "email@email.com";
-            this.userStatus.Label = "Guest Account";
-        }//called when the sign in button is clicked
+            switch (signIn())
+            {
+                case "passed":
+
+                    this.usernameEditBox.Enabled = false;
+                    this.passwordEditBox.Enabled = false;
+
+                    this.userStatus.Label = "Guest Account";
+                    //if username and password passed the login
+                    break;
+                case "invalid":
+                    this.userStatus.Label = "Invalid User Name/Password";
+                    //if the username/password is invalid
+                    return;
+                case "noserver":
+                    this.userStatus.Label = "No server connection";
+                    //if no server connection in one form or another is detected
+                    return;
+            }
+            this.signInButton.Visible = false;
+            this.signOutButton.Visible = true;
+            //Swap the sign in buttons
+
+            if (passwordEditBox.Text != "****")
+            {
+                password = passwordEditBox.Text;
+                passwordEditBox.Text = "****";
+
+            }
+            //retrieve and protect the user's password
+
+            userName = usernameEditBox.Text;
+
+            //retrieve the user's username
+
+
+            
+
+        }
+        private void signOutButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            userName.Remove(0);
+            password.Remove(0);
+            this.usernameEditBox.Text = "";
+            this.passwordEditBox.Text = "";
+            this.usernameEditBox.Enabled = true;
+            this.passwordEditBox.Enabled = true;
+        }
+
+        
+
+
+        private void addLink_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
 
         #endregion
+        #endregion 
 
+        #region Restrictions ribbon section
+        private void presetDropDown_SelectionChanged(object sender, RibbonControlEventArgs e)
+        {
 
-
+        }
+        #endregion
 
     }
 }
