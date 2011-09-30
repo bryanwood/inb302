5a35d31061401785a27a7884ea548f4449ab117e
Carl <Carl@Win7-MacBook.francia.home>
2011-09-30 22:22:30 +1000

    Ported over the proper way to handle authentication to the login/create pages.

---
+++ UFiles.Web/Scripts/JS/site.auth.js
@@ -0,0 +1,27 @@
+﻿function AjaxValidateAndGo(caller, postURL, validateDiv) {
+
+var serialized = $(caller).serialize();
+
+$.ajax({
+    type: "POST",
+    url: postURL,
+    data: serialized,
+    statusCode: {
+
+    201: function() {
+    var reply = $.parseJSON(data.responseText);
+    window.location.assign(reply.GoTo);
+    },
+    400: function(data) {
+    var reply = $.parseJSON(data.responseText);
+    $(validateDiv).html(reply.FailureReason);
+    $(validateDiv).slideDown(500);
+    $(validateDiv).animate({ opacity: 1.0 }, 500);
+
+    }
+
+    }
+    })
+return false;
+
+}
