37ee7b8d5c9dd03ed195fc06681480e8eaf79d1a
Carl <Carl@Win7-MacBook.francia.home>
2011-10-03 23:38:08 +1000

    Added Overview.cshtml, as well as associated controller and models.

--- UFiles.Web/Controllers/HomeController.cs
+++ UFiles.Web/Controllers/HomeController.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using UFiles.Domain.Abstract;
+using UFiles.Domain.Concrete;
 using UFiles.Domain.Entities;
+using UFiles.Web.Models;
 namespace UFiles.Web.Controllers
 {
     public class HomeController : Controller
@@ -17,7 +17,36 @@ namespace UFiles.Web.Controllers
 
         public ActionResult Overview()
         {
-            return View();
+            OverviewModel overviewModel = new OverviewModel();
+            IUserService userService = new UserService();
+
+            User currentUser = userService.GetUserByEmail(User.Identity.Name);
+
+            overviewModel.LoggedInUser.FName = currentUser.FirstName;
+            overviewModel.LoggedInUser.LName = currentUser.LastName;
+            overviewModel.LoggedInUser.Email = currentUser.Email;
+
+            // Test data -- Delete after logging in works.
+            TransmittalListingModel testTransmittal = new TransmittalListingModel();
+            testTransmittal.FileName = "Test File.doc";
+            testTransmittal.Email = "important.guy@example.com";
+            testTransmittal.SentDate = DateTime.Now;
+            testTransmittal.DownloadLink = "ABC";
+
+            overviewModel.RecentlySentTransmittals.Add(testTransmittal);
+
+            testTransmittal = new TransmittalListingModel();
+
+            testTransmittal.FileName = "World Domination Plans.xls";
+            testTransmittal.Email = "doctor.evil@example.com";
+            testTransmittal.SentDate = DateTime.Now;
+            testTransmittal.DownloadLink = "ABB";
+
+            overviewModel.RecentlySentTransmittals.Add(testTransmittal);
+
+            // End test data
+
+            return View(overviewModel);
         }
 
         public ActionResult About()
---
+++ UFiles.Web/Models/UserModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using UFiles.Domain.Entities;
+
+namespace UFiles.Web.Models
+{
+    public class UserModel
+    {
+        public int ID { get; set; }
+        public Role Role { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Email { get; set; }
+    }
+}
