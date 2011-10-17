ffb3f83d31207aa8ecbd5c2dc62a0d24a6180b57
Bryan <Bryan@Bryan-HP.qut.edu.au>
2011-10-17 13:48:07 +1000

    Admin ui work IUFileService started

---
+++ UFiles.Web/Areas/Administration/Controllers/LogController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UFiles.Domain.Concrete;
+using UFiles.Domain.Entities;
+using System.Data.Entity;
+
+namespace UFiles.Web.Areas.Administration.Controllers
+{
+    public class LogController : Controller
+    {
+        private UFileContext db = new UFileContext();
+        //
+        // GET: /Administration/Log/
+
+        public ActionResult Users()
+        {
+            //TODO: Include
+            return View(db.Events.OfType<UserEvent>().Include(x=>x.User).ToList());
+        }
+        public ActionResult Transmittals()
+        {
+            return View(db.Events.OfType<TransmittalEvent>().Include(x=>x.Transmittal).ToList());
+        }
+        public ActionResult Files()
+        {
+            return View(db.Events.OfType<FileAccessEvent>().Include(x=>x.File).Include(x=>x.User).ToList());
+        }
+
+    }
+}
---
+++ UFiles.Web/UFileService.svc.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using UFiles.Domain.Concrete;
+using UFiles.Domain.Entities;
+
+namespace UFiles.Web
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "UFileService" in code, svc and config file together.
+    public class UFileService : IUFileService
+    {
+        UFileContext db = new UFileContext();
+
+        public int Login(string email, string password)
+        {
+            return db.Users.Where(x => x.Email == email && x.PasswordHash == password).Single().UserId;
+        }
+
+        public int NewTransmittal(int userId)
+        {
+            var transmittal = new Transmittal();
+            db.Transmittals.Add(transmittal);
+            db.SaveChanges();
+            return transmittal.TransmittalId;
+        }
+
+        public void AddFile(int userId, int transmittalId, string fileName, string fileType, byte[] fileData)
+        {
+            var filedata = new FileData();
+            filedata.Size = fileData.Length;
+            filedata.Extension = fileType;
+            var file = new File();
+            file.Name = fileName;
+            file.Owner = db.Users.Find(userId);
+            file.Transmittals.Add(db.Transmittals.Find(transmittalId));
+            db.Files.Add(file);
+            db.FileDatas.Add(filedata);
+            db.SaveChanges();
+
+        }
+
+        public void AddRecipients(int transmittalId, string[] recipients)
+        {
+            foreach(var recipient in recipients){
+                try
+                {
+                    var existing = db.Users.Where(x => x.Email == recipient).Single();
+
+                    db.Transmittals.Find(transmittalId).Recipients.Add(existing);
+                }
+                catch
+                {
+                    db.Transmittals.Find(transmittalId).Recipients.Add(new User
+                    {
+                        Email = recipient,
+                        PasswordHash = new Random().Next(1000,9999).ToString(),
+                        Verified  =false,
+                        VerifiedHash = new Random().Next(100000,999999).ToString(),
+                        Role = db.Roles.First()
+                    });
+                }
+        }
+            db.SaveChanges();
+        }
+
+        public void SendTransmittal(int transmittalId)
+        {
+            
+        }
+    }
+}
